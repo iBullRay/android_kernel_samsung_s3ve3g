e728e3397528e892e8c9f0756d5156e9ba86cdc0
Unknown <bullray097@yandex.ru>
2018-07-05 14:12:59 +0300

    Merge tag 'v3.4.113' into s3ve3gxx
    This is the 3.4.113 stable release
    
    Signed-off-by: iBullRay <bullray097@yandex.ru>

--- net/wireless/core.h
+++ net/wireless/core.h
@@ -67,6 +67,10 @@ struct cfg80211_registered_device {
 	struct work_struct scan_done_wk;
 	struct work_struct sched_scan_results_wk;
 
+#ifdef CONFIG_NL80211_TESTMODE
+	struct genl_info *testmode_info;
+#endif
+
 	struct mutex sched_scan_mtx;
 
 	struct genl_info *cur_cmd_info;
@@ -424,6 +428,7 @@ int cfg80211_change_iface(struct cfg80211_registered_device *rdev,
 			  struct net_device *dev, enum nl80211_iftype ntype,
 			  u32 *flags, struct vif_params *params);
 void cfg80211_process_rdev_events(struct cfg80211_registered_device *rdev);
+void cfg80211_process_wdev_events(struct wireless_dev *wdev);
 
 int cfg80211_can_change_interface(struct cfg80211_registered_device *rdev,
 				  struct wireless_dev *wdev,
