02933de8589dc65003865e1d1086091f86854d6a
Unknown <bullray097@yandex.ru>
2018-07-08 11:47:42 +0300

    drivers: mmc: Fix unused variable

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -3623,7 +3623,6 @@ int mmc_pm_notify(struct notifier_block *notify_block,
 	struct mmc_host *host = container_of(
 		notify_block, struct mmc_host, pm_notify);
 	unsigned long flags;
-	int err = 0;
 
 	switch (mode) {
 	case PM_HIBERNATION_PREPARE:
