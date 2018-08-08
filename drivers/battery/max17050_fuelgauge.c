db6bb307d33346924e41b0ee65d03305098e780e
Unknown <bullray097@yandex.ru>
2018-08-08 12:44:07 +0300

    max17050_fuelgauge: Migrate to alarmtimer interface
    Change-Id: I7c80d1c2d8cdadcc4503d41999bf215b6faddade

--- drivers/battery/max17050_fuelgauge.c
+++ drivers/battery/max17050_fuelgauge.c
@@ -2043,7 +2043,6 @@ static int get_fuelgauge_soc(struct i2c_client *client)
 	int fg_vcell;
 	int fg_current;
 	int avg_current;
-	ktime_t	current_time;
 	struct timespec ts;
 	int fullcap_check_interval;
 
@@ -2053,8 +2052,7 @@ static int get_fuelgauge_soc(struct i2c_client *client)
 			goto return_soc;
 		}
 
-	current_time = alarm_get_elapsed_realtime();
-	ts = ktime_to_timespec(current_time);
+	get_monotonic_boottime(&ts);
 
 	/* check fullcap range */
 	fullcap_check_interval =
@@ -2196,11 +2194,9 @@ bool sec_hal_fg_init(struct i2c_client *client)
 {
 	struct sec_fuelgauge_info *fuelgauge =
 				i2c_get_clientdata(client);
-	ktime_t	current_time;
 	struct timespec ts;
 
-	current_time = alarm_get_elapsed_realtime();
-	ts = ktime_to_timespec(current_time);
+	get_monotonic_boottime(&ts);
 
 	board_fuelgauge_init(fuelgauge);
 
