963549f58f8909b1c3812fc8598a70a7e9729c0b
Unknown <bullray097@yandex.ru>
2018-09-13 17:28:15 +0300

    Revert "Merge CAF 'LA.BF.1.1.3_rb1.15' into cm-14.1"
    This reverts commit 3fdc74a0b573533826dc8bdcc98b900bb5ac8f05.
    
    * Will do upstream with cherry-picking or some minor merges as it is more effective.
    
    Signed-off-by: iBullRay <bullray097@yandex.ru>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -532,13 +532,13 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 
 		if (lowmem_debug_level >= 2 && selected_oom_score_adj == 0) {
             		show_mem(SHOW_MEM_FILTER_NODES);
-                    dump_tasks(NULL, NULL);
+					dump_tasks(NULL, NULL);
             		show_mem_call_notifiers();
 		}
 
 		lowmem_deathpending_timeout = jiffies + HZ;
+		send_sig(SIGKILL, selected, 0);
 		set_tsk_thread_flag(selected, TIF_MEMDIE);
-        send_sig(SIGKILL, selected, 0);
 		rem -= selected_tasksize;
 		rcu_read_unlock();
 		/* give the system time to free up the memory */
